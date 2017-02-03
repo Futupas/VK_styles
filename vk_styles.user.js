8f0cf3112c939ca8373c4492e9dfb8748de1e08e
Olexandr Panov <futupas@gmail.com>
2017-02-03 23:24:25 +0200

    first commit

---
+++ vk_styles.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name VK_styles
+// @description Makes Vk for me, with my styles
+// @author Olexandr Panov
+// @license MIT
+// @version 2.0
+// @include https://vk.com/*
+// ==/UserScript==
+
+var style_1_href = 'vk-sites.bitballoon.com/styles_0.css';
+var style_1 = document.createElement('link');
+  style_1.setAttribute('rel', 'stylesheet');
+  style_1.setAttribute('href', style_1_href);
+
+  document.querySelector('head').appendChild(style_1);
