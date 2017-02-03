9564ba7ed6e65326833ff3194c72db23fbacc765
Olexandr Panov <futupas@gmail.com>
2017-02-03 23:32:29 +0200

    version 0.0.1

--- vk_styles.user.js
+++ vk_styles.user.js
@@ -7,7 +7,7 @@
 // @include https://vk.com/*
 // ==/UserScript==
 
-var style_1_href = 'vk-sites.bitballoon.com/styles_0.css';
+var style_1_href = 'http://vk-sites.bitballoon.com/styles_0.css';
 var style_1 = document.createElement('link');
   style_1.setAttribute('rel', 'stylesheet');
   style_1.setAttribute('href', style_1_href);
